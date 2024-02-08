13b6f74e7d9e7a25e385f5b2df75ff109737b525
Waqas Ahmed <wa02263@gmail.com>
2024-02-08 01:12:42 -0800

    initialized

--- priority_simulation.dart
+++ priority_simulation.dart
@@ -25,11 +25,13 @@ class Customer {
   final int interarrival;
   final int serviceTime;
   final int priority;
-  int startTime = 0;
-  int endTime = 0;
+  final int arrival;
+  int? startTime;
+  int? endTime;
   int servedTime = 0;
 
-  Customer(this.customer, this.interarrival, this.serviceTime, this.priority);
+  Customer(this.customer, this.interarrival, this.serviceTime, this.priority,
+      this.arrival);
 }
 
 class PrioritySimulation {
@@ -43,58 +45,103 @@ class PrioritySimulation {
         customerData['interarrival']!,
         customerData['serviceTime']!,
         customerData['priority']!,
+        customerData['arrival']!,
       );
       customers.add(newCustomer);
     }
   }
 
   void simulate() {
-    customers.sort((a, b) => b.priority.compareTo(a.priority)); // Sort customers by priority (highest first)
+    // customers.sort((a, b) => b.priority.compareTo(a.priority)); // Sort customers by priority (highest first)
     int currentTime = 0;
-    int currentPriorityIndex = 0;
-
-    while (true) {
-      // Serve the highest priority customer if they are already waiting
-      while (currentPriorityIndex < customers.length &&
-          customers[currentPriorityIndex].interarrival <= currentTime) {
-        var currentCustomer = customers[currentPriorityIndex];
-        if (currentCustomer.servedTime == 0) {
-          currentCustomer.startTime = currentTime; // Set start time
+    for (int i = 0; i < customers.length; i++) {
+      while (true) {
+        if (customers[i].priority == 1 &&
+            // customers[i].arrival == currentTime &&
+            customers[i].servedTime < customers[i].serviceTime) {
+          customers[i].servedTime += customers[i].serviceTime;
+          currentTime += customers[i].serviceTime;
         }
-        currentCustomer.servedTime++;
-        currentTime++;
-        if (currentCustomer.servedTime == currentCustomer.serviceTime) {
-          currentCustomer.endTime = currentTime; // Set end time
-          currentCustomer.servedTime = 0; // Reset served time
-          currentPriorityIndex++;
+         else if (customers[i].priority == 2 &&
+            // customers[i].arrival == currentTime &&
+            customers[i].servedTime < customers[i].serviceTime) {
+          customers[i].servedTime += 1;
+          currentTime += 1;
         }
-      }
-
-      // Check if all customers are served
-      if (currentPriorityIndex == customers.length &&
-          customers.every((customer) => customer.endTime != 0)) {
-        break;
-      }
-
-      // If no customers are waiting, advance time to the next arrival
-      if (currentPriorityIndex >= customers.length || customers[currentPriorityIndex].interarrival > currentTime) {
-        currentTime = customers[currentPriorityIndex].interarrival;
+         else if (customers[i].priority == 3 &&
+            // customers[i].arrival == currentTime &&
+            customers[i].servedTime < customers[i].serviceTime) {
+          customers[i].servedTime += 1;
+          currentTime += 1;
+        }
+        if(customers.forEach((key,value) {ke}))
       }
     }
+    // int currentPriorityIndex = 0;
 
-    // Print arrival, start, and end times for each customer
-    for (var customer in customers) {
-      print('Customer ${customer.customer}: Start Time - ${customer.startTime}, End Time - ${customer.endTime}');
-    }
+    //   while (true) {
+    //     // Serve the highest priority customer if they are already waiting
+    //     while (currentPriorityIndex < customers.length &&
+    //         customers[currentPriorityIndex].interarrival <= currentTime) {
+    //       var currentCustomer = customers[currentPriorityIndex];
+    //       if (currentCustomer.servedTime == 0) {
+    //         currentCustomer.startTime = currentTime; // Set start time
+    //       }
+    //       currentCustomer.servedTime++;
+    //       currentTime++;
+    //       if (currentCustomer.servedTime == currentCustomer.serviceTime) {
+    //         currentCustomer.endTime = currentTime; // Set end time
+    //         currentCustomer.servedTime = 0; // Reset served time
+    //         currentPriorityIndex++;
+    //       }
+    //     }
+
+    //     // Check if all customers are served
+    //     if (currentPriorityIndex == customers.length &&
+    //         customers.every((customer) => customer.endTime != 0)) {
+    //       break;
+    //     }
+
+    //     // If no customers are waiting, advance time to the next arrival
+    //     if (currentPriorityIndex >= customers.length || customers[currentPriorityIndex].interarrival > currentTime) {
+    //       currentTime = customers[currentPriorityIndex].interarrival;
+    //     }
+    //   }
+
+    //   // Print arrival, start, and end times for each customer
+    //   for (var customer in customers) {
+    //     print('Customer ${customer.customer}: Start Time - ${customer.startTime}, End Time - ${customer.endTime}');
+    //   }
+    // }
   }
 }
 
 void main() {
   List<Map<String, int>> data = [
-    {'customer': 1, 'interarrival': 4, 'serviceTime': 7, 'priority': 2},
-    {'customer': 2, 'interarrival': 3, 'serviceTime': 9, 'priority': 3}
+    {
+      'customer': 1,
+      'interarrival': 4,
+      'serviceTime': 7,
+      'priority': 2,
+      "arrival": 2
+    },
+    {
+      'customer': 2,
+      'interarrival': 3,
+      'serviceTime': 9,
+      'priority': 3,
+      "arrival": 2
+    },
+    {
+      'customer': 3,
+      'interarrival': 3,
+      'serviceTime': 9,
+      'priority': 1,
+      "arrival": 3
+    },
   ];
 
   var prioritySimulation = PrioritySimulation(data);
+  print(prioritySimulation.customers);
   prioritySimulation.simulate();
 }
